f36d8c9aa1c5652175c7897141eb3dc3a79d1b85
ksnowman <113837957+ksnowman@users.noreply.github.com>
2022-11-23 22:46:08 +0900

    1
    1

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { Client, Reservation } = require('../models');
+const path = require('path');
 
 const router = express.Router();
+const PUBLIC = path.join(__dirname, '../views');
 
 router.get('/clients', async (req, res, next) => {
     try {
@@ -27,7 +29,31 @@ router.get('/reservations', async (req, res, next) => {
 
 router.post('/signup', async (req, res, next) => {
     try {
-       res.sendFile()
+        res.sendFile(path.join(PUBLIC, "signUp.html"))
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+});
+
+router.post('/signUp/validation', async (req, res, next) => {
+    try {
+        const { id, password, name, birth, gender } = req.body;
+        const client = await Client.findOne({where:{id}});
+        if (user) {
+            next('이미 등록된 사용자 아이디입니다.');
+            return;
+        }
+
+        try {
+            const hash = await bcrypt.hash(password, 12);
+            await User.create({
+                id,
+                password: hash,
+                name,
+                birth,
+                gender
+            });
     } catch (err) {
         console.error(err);
         next(err);
