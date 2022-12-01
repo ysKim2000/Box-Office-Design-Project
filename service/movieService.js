2aacbdfce6aced0f62b1e58aff1d56e8ed83832d
Yunseo <kys051348@gmail.com>
2022-12-01 17:47:09 +0900

    오타수정

--- service/movieService.js
+++ service/movieService.js
@@ -1,12 +1,12 @@
-const movieRepositiry = require("../repository/movieRepository");
+const movieRepository = require("../repository/movieRepository");
 
 exports.createTicket = async (ticket, userId, movieCode, movieName, movieTime, movieSeat) => {
     
     const regex = /^[a-e|A-E|0-5]+$/;
     if (regex.test(movieSeat) == false) throw '잘못된 좌석입니다.'
     
-    const isVacant = movieRepositiry.checkTicket(ticket);
+    const isVacant = movieRepository.checkTicket(ticket);
     if (!isVacant) throw "이미 등록된 좌석입니다."
 
-    await movieRepositiry.createTicket(ticket, userId, movieCode, movieName, movieTime, movieSeat);
+    await movieRepository.createTicket(ticket, userId, movieCode, movieName, movieTime, movieSeat);
 };
